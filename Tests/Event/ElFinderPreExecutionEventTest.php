146ea4d97787258da568d78b98c463e2a4384cc6
Nicolas MURE <n.mure@local.fr>
2015-06-09 17:26:45 +0200

    Events unit tests

---
+++ Tests/Event/ElFinderPreExecutionEventTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FM\ElfinderBundle\Tests\Event;
+
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use FM\ElfinderBundle\Event\ElFinderPreExecutionEvent;
+
+class ElFinderPreExecutionEventTest extends TestCase
+{
+    public function testGetCommand()
+    {
+        $command = 'rm';
+        $request = new Request(array('cmd' => $command));
+        $httpKernel = $this->getMock('Symfony\Component\HttpKernel\HttpKernelInterface');
+        $event = new ElFinderPreExecutionEvent($request, $httpKernel, 'testInstance', 'testHomeFolder');
+        $this->assertEquals($command, $event->getCommand());
+    }
+
+    public function testSubRequest()
+    {
+        $request = new Request(array('cmd' => 'info'));
+        $httpKernel = $this->getMock('Symfony\Component\HttpKernel\HttpKernelInterface');
+        $httpKernel
+            ->expects($this->once())
+            ->method('handle');
+        $event = new ElFinderPreExecutionEvent($request, $httpKernel, 'testInstance', 'testHomeFolder');
+        
+        $jsonResponse = $event->subRequest(array(
+            'instance' => $event->getInstance(),
+            'homeFolder' => $event->getHomeFolder()
+        ), $request->query->all());
+    }
+}
