33f50a1693765394b8b41081c7fafdb8366822e2
Al Ganiev <helios.ag@gmail.com>
2015-12-10 14:49:18 +1000

    test rewrite

--- Tests/DependencyInjection/FMElfinderExtensionTest.php
+++ Tests/DependencyInjection/FMElfinderExtensionTest.php
@@ -43,22 +43,20 @@ class FMElfinderExtensionTest extends AbstractExtensionTestCase
     protected function getMinimalConfiguration()
     {
         $yaml = <<<EOF
-fm_elfinder:
-    instances:
-        default:
-          locale: %locale%
-          editor: simple # other choices are tinymce or simple
-          include_assets: true
-          fullscreen: true
-          connector:
-              debug: true # defaults to false
-              roots:       # at least one root must be defined
-                  uploads:
-                      driver: LocalFileSystem
-                      path: uploads
-                      upload_allow: ['image/png', 'image/jpg', 'image/jpeg']
-                      upload_deny: ['all']
-                      upload_max_size: 2M
+default:
+  locale: %locale%
+  editor: simple # other choices are tinymce or simple
+  include_assets: true
+  fullscreen: true
+  connector:
+      debug: true # defaults to false
+      roots:       # at least one root must be defined
+          uploads:
+              driver: LocalFileSystem
+              path: uploads
+              upload_allow: ['image/png', 'image/jpg', 'image/jpeg']
+              upload_deny: ['all']
+              upload_max_size: 2M
 EOF;
         $parser = new Parser();
 
