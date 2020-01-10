fa63f31c1d6fb3d6d316559316211bc4d90d25b2
Al Ganiev <helios.ag@gmail.com>
2020-01-11 00:11:21 +1000

    Some fixes

---
+++ src/Command/ElFinderInstallerCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FM\ElfinderBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+final class ElFinderInstallerCommand extends Command
+{
+
+    private const ELFINDER_CSS_DIR    = 'vendor/studio-42/elfinder/css';
+    private const ELFINDER_JS_DIR     = 'vendor/studio-42/elfinder/js';
+    private const ELFINDER_SOUNDS_DIR = 'vendor/studio-42/elfinder/sounds';
+    private const ELFINDER_IMG_DIR    = 'vendor/studio-42/elfinder/img';
+
+    protected static $defaultName = 'elfinder:install';
+
+    protected $fileSystem;
+    protected $parameterBag;
+
+    public function __construct(Filesystem $filesystem, ParameterBagInterface $parameterBag)
+    {
+        $this->fileSystem = $filesystem;
+        $this->parameterBag = $parameterBag;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Copies elfinder assets to public directory')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $publicDir = $this->parameterBag->get('kernel.project_dir') . '/public/elfinder';
+
+        $this->fileSystem->mirror(self::ELFINDER_CSS_DIR, $publicDir . '/css');
+        $this->fileSystem->mirror(self::ELFINDER_IMG_DIR, $publicDir . '/img');
+        $this->fileSystem->mirror(self::ELFINDER_JS_DIR, $publicDir . '/js');
+        $this->fileSystem->mirror(self::ELFINDER_SOUNDS_DIR, $publicDir . '/sounds');
+
+        $output->writeln('elFinder assets successfully installed');
+
+        return 0;
+    }
+}
