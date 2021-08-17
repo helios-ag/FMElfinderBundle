281cd5c3618589041fb672d7b56fea955f31f4e3
Al Ganiev <helios.ag@gmail.com>
2021-08-17 23:20:39 +1000

    Tweaks

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,9 +1,3 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
-
 require_once __DIR__.'/../vendor/autoload.php';
-
-if (!class_exists(TestCase::class)) {
-    class_alias(\PHPUnit_Framework_TestCase::class, TestCase::class);
-}
