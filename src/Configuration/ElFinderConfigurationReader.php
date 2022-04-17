e8df6b1596d7927d2aaf6d32d3a90049ecca309f
entobvba <joran@ento.be>
2022-04-17 04:22:49 +0200

    fix plugin options not passed (#478)
    * fix plugin options not passed
    
    * fix plugin options not passed

--- src/Configuration/ElFinderConfigurationReader.php
+++ src/Configuration/ElFinderConfigurationReader.php
@@ -85,7 +85,7 @@ class ElFinderConfigurationReader implements ElFinderConfigurationProviderInterf
                 'service'           => $driver,
                 'glideURL'          => $parameter['glide_url'],
                 'glideKey'          => $parameter['glide_key'],
-                'plugin'            => $parameter['plugins'],
+                'plugin'            => $options['plugin'],
                 'path'              => $pathAndHomeFolder,
                 'startPath'         => $parameter['start_path'],
                 'encoding'          => $parameter['encoding'],
